ffe9e218b5b6b632fc1a087fe0e232bf76a24082
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-24 11:19:26 -0700

    changes made

--- 0x00-shell_basics/4-puts.c
+++ 0x00-shell_basics/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *Entry -main
+ *
+ *Puts unction - prints string
+ *Always returns zero - success
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
